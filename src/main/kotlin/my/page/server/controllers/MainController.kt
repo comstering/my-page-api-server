11039edff0d24a0d89eb324d6503feca8d6b3172
comstering <coms1101@naver.com>
2022-04-28 16:40:15 +0900

    ♻️ refactor: Remove semicolon

--- src/main/kotlin/my/page/server/controllers/MainController.kt
+++ src/main/kotlin/my/page/server/controllers/MainController.kt
@@ -1,8 +1,8 @@
 package my.page.server.controllers
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 public class MainController {
