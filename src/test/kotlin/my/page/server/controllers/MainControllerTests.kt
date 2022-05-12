2f7bc37f8f3e0cf82b63a34482508ef7e72a8637
comstering <coms1101@naver.com>
2022-05-12 20:33:43 +0900

    ♻️ fix: Removed don't use import code

--- src/test/kotlin/my/page/server/controllers/MainControllerTests.kt
+++ src/test/kotlin/my/page/server/controllers/MainControllerTests.kt
@@ -1,8 +1,5 @@
 package my.page.server.controllers
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import my.page.server.models.Project
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
--- src/test/kotlin/my/page/server/controllers/ProjectControllerTests.kt
+++ src/test/kotlin/my/page/server/controllers/ProjectControllerTests.kt
@@ -1,6 +1,5 @@
 package my.page.server.controllers
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
@@ -9,11 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 
 @SpringBootTest
 @AutoConfigureMockMvc
